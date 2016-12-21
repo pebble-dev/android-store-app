4b9c3b33f3290fb0bee16e58e684a5fe802f3ad5
qichuan <qichuan@zhangqichuan.com>
2016-12-21 14:13:25 +0800

    added Fabric

--- app/src/main/java/io/rebble/store/activity/SplashActivity.java
+++ app/src/main/java/io/rebble/store/activity/SplashActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 import io.rebble.store.R;
 
 /**
@@ -15,6 +17,7 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_splash);
 
         //TODO initialization tasks
